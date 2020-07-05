7f349b1741a9be5c8ad1c41a4aaf2d9f3b487113
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2020-07-04 22:54:16 -0300

    Fixed CORS configuration and lambda response

--- src/functions/api-handler.js
+++ src/functions/api-handler.js
@@ -2,10 +2,16 @@
 
 require('dotenv').config();
 
+const responseHeaders = process.env.CORS_ALLOWED_ORIGIN ? {
+	'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+	'Access-Control-Allow-Origin': process.env.CORS_ALLOWED_ORIGIN,
+	'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PUT,POST,DELETE'
+} : {};
+
 module.exports = api => async event => {
 	try {
 		const result = await api(event);
-		return { body: JSON.stringify(result) };
+		return { body: JSON.stringify(result), headers: responseHeaders };
 	} catch(e) {
 		throw new Error(`An error ocurred: ${e.message}`);
 	}
